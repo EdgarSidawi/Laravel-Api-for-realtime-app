585dec1131583fec096fa46e5fc803f6aed1d17b
Edgar Sidawi <romantibuai@gmail.com>
2019-10-07 18:35:29 +0000

    replies migration table

--- database/migrations/2019_10_05_202941_create_questions_table.php
+++ database/migrations/2019_10_05_202941_create_questions_table.php
@@ -18,6 +18,9 @@ class CreateQuestionsTable extends Migration
             $table->string('title');
             $table->string('slug');
             $table->text('body');
+
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
     }
