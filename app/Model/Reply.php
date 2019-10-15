72a82c2f01b7c2b32b28cd8fa80fdc4dd7ea0456
Edgar Sidawi <romantibuai@gmail.com>
2019-10-15 18:40:02 +0000

    reply user relationship

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,11 +3,17 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Reply extends Model
 {
     public function question()
     {
         return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(user::class);
     }
 }
