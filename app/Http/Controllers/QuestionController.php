74d32874264e89117cb6f27010423df4d3cf7b6e
Edgar Sidawi <romantibuai@gmail.com>
2019-10-22 17:00:26 +0000

    Question resource controller @show

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -35,9 +35,9 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Question $question)
     {
-        //
+        return $question;
     }
 
     /**
