e55b1cb7ef2e703e9e08727e142340a7f6c8becb
Edgar Sidawi <romantibuai@gmail.com>
2019-10-22 17:19:16 +0000

    Question resource controller@destroy

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -58,8 +58,9 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response('Deleted', 200);
     }
 }
