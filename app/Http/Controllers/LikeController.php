00740d8c3da85b9e33a850d36a398064735d2fde
Edgar Sidawi <romantibuai@gmail.com>
2019-10-24 11:03:55 +0000

    like controller@unLikeIt updated

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -18,6 +18,7 @@ class LikeController extends Controller
 
     public function unLikeIt(Reply $reply)
     {
-        $reply->like()->where(['user_id', auth()->id()])->first()->delete();
+        // $reply->like()->where(['user_id' => auth()->id()])->first()->delete();
+        $reply->like()->where(['user_id' => 1])->first()->delete();
     }
 }
