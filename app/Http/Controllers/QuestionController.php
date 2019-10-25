864659b0b1d4cf4ce22e7af2106713abca5403a0
Edgar Sidawi <romantibuai@gmail.com>
2019-10-25 18:51:02 +0000

    added JWT authentication to all routes

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -9,6 +9,17 @@ use Illuminate\Http\Response;
 
 class QuestionController extends Controller
 {
+
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('JWT', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
