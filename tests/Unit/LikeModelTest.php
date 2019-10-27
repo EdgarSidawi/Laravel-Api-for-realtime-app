f2fc4e9083759afcd71aac572363bfcab2cbf8b8
Edgar Sidawi <romantibuai@gmail.com>
2019-10-27 19:17:59 +0000

    created Like model test file and tests

---
+++ tests/Unit/LikeModelTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Model\Like;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LikeModelTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function like_has_reply_id()
+    {
+        $like = Like::create([
+            'reply_id' => 1,
+            'user_id' => 1
+        ]);
+
+        $this->assertEquals(1, $like->reply_id);
+    }
+
+    /** @test */
+    public function like_has_user_id()
+    {
+        $like = Like::create([
+            'reply_id' => 1,
+            'user_id' => 1
+        ]);
+
+        $this->assertEquals(1, $like->user_id);
+    }
+}
