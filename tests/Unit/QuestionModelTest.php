df5f2fcd3245054c6f207d2c51463a4f3a792fb2
Edgar Sidawi <romantibuai@gmail.com>
2019-10-27 19:27:47 +0000

    question model test for title attribute

--- tests/Unit/QuestionModelTest.php
+++ tests/Unit/QuestionModelTest.php
@@ -2,13 +2,21 @@
 
 namespace Tests\Unit;
 
+use App\Model\Question;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class QuestionModelTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /** @test */
     public function question_has_title_attribute()
-    { }
+    {
+        $question = factory(Question::class)->create();
+
+        $this->assertEquals($question->title, $question->title);
+    }
 }
