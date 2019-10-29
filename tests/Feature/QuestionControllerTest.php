937492d61288f93baa348cf00e87f6e4aa56a0dc
Edgar Sidawi <romantibuai@gmail.com>
2019-10-29 18:53:44 +0000

    question controller test for show method

--- tests/Feature/QuestionControllerTest.php
+++ tests/Feature/QuestionControllerTest.php
@@ -20,4 +20,16 @@ class QuestionControllerTest extends TestCase
 
         $response->assertSuccessful()->assertStatus(200)->assertJsonCount(1);
     }
+
+    /** @test */
+    public function user_can_get_a_question()
+    {
+        $question = factory(Question::class)->create();
+
+        $response = $this->json('GET', "/api/question/{$question->slug}");
+
+        $response->assertStatus(200);
+        $response->assertSuccessful();
+        $response->assertJsonCount(1);
+    }
 }
