40eb8675033e2f27a0569ecae4acd2a3fc7e0bb4
Edgar Sidawi <romantibuai@gmail.com>
2019-10-31 16:08:19 +0000

    fixed categorycontroller test

--- tests/Feature/CategoryControllerTest.php
+++ tests/Feature/CategoryControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature;
 
 use App\Model\Category;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -15,40 +14,37 @@ class CategoryControllerTest extends TestCase
     // use WithoutMiddleware;
 
 
+    /** @test */
+    public function user_can_get_all_categories()
+    {
+        $response = $this->json('GET', '/api/category');
+        // dd($response);
+        $response->assertStatus(200)->assertJsonCount(1);
+    }
 
     /** @test */
-    // public function user_can_get_all_categories()
-    // {
-    //     $response = $this->json('GET', '/api/category');
+    public function user_can_get_a_category()
+    {
+        $category = factory(Category::class)->create();
 
-    //     $response->assertStatus(200)->assertJsonCount(1);
-    // }
+        $response = $this->json('GET', "/api/category/{$category->slug}");
 
-    /** @test */
-    // public function user_can_get_a_category()
-    // {
-    //     $category = factory(Category::class)->create();
-
-    //     $response = $this->json('GET', "/api/category/{$category->slug}");
-
-    //     $response->assertStatus(200);
-    //     $response->assertSuccessful();
-    //     $response->assertJsonCount(1);
-    // }
+        $response->assertStatus(200)->assertSuccessful()->assertJsonCount(1);
+    }
 
     /** @test */
-    // public function user_can_not_create_category_if_unauthenticated_or_no_token_available()
-    // {
-    //     $data = [
-    //         'name' => 'i love laravel',
-    //         'slug' => 'i-love-laravel'
-    //     ];
-    //     $header = [
-    //         'Content-Type' => 'application/json',
-    //         'Authorization' => ''
-    //     ];
+    public function user_can_not_create_category_if_unauthenticated_or_no_token_available()
+    {
+        $data = [
+            'name' => 'i love laravel',
+            'slug' => 'i-love-laravel'
+        ];
+        $header = [
+            'Content-Type' => 'application/json',
+            'Authorization' => ''
+        ];
 
-    //     $response = $this->json('POST', '/api/category', $data, $header);
-    //     $response->assertStatus(400);
-    // }
+        $response = $this->json('POST', '/api/category', $data, $header);
+        $response->assertStatus(400);
+    }
 }
