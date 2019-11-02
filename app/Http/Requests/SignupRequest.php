4d8fcdad196de6f9e8e8e8b18865dc9a94c6119f
Edgar Sidawi <romantibuai@gmail.com>
2019-11-02 19:51:38 +0000

    signup request info

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -13,7 +13,7 @@ class SignupRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class SignupRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required|confirmed',
+
         ];
     }
 }
