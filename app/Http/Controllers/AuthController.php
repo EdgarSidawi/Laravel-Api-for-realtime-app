f57e18e2a0a07017f83af526683094d5158366a5
Edgar Sidawi <romantibuai@gmail.com>
2019-11-07 16:52:00 +0000

    updated auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -85,7 +85,8 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()->name
+            'user' => auth()->user()->name,
+            'user_id' => auth()->user()->id
         ]);
     }
 }
