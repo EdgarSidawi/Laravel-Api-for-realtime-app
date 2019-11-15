186159245bcca8802fce760a3092b33f1ae49b68
Edgar Sidawi <romantibuai@gmail.com>
2019-11-15 18:55:45 +0000

    reply notification constructor

--- app/Notifications/NewReplyNotification.php
+++ app/Notifications/NewReplyNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Model\Reply;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -11,14 +12,16 @@ class NewReplyNotification extends Notification
 {
     use Queueable;
 
+    public $reply;
+
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Reply $reply)
     {
-        //
+        $this->reply = $reply;
     }
 
     /**
@@ -29,21 +32,7 @@ class NewReplyNotification extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        return ['database'];
     }
 
     /**
