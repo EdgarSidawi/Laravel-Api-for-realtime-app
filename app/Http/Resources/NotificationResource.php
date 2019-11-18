635502b561862b198dc46082bbf944b1d9ac3bcb
Edgar Sidawi <romantibuai@gmail.com>
2019-11-18 13:52:42 +0000

    notification resource value

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -14,6 +14,11 @@ class NotificationResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'replyBy' => $this->data['replyBy'],
+            'question' => $this->data['question'],
+            'path' => $this->data['path'],
+        ];
     }
 }
