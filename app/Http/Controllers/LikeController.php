07f6e234ca074f8ef049539d3c873b982529d9b2
Edgar Sidawi <romantibuai@gmail.com>
2019-11-18 17:40:25 +0000

    new broadcast like event

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\LikeEvent;
 use App\Model\Like;
 use App\Model\Reply;
 use Illuminate\Http\Request;
@@ -24,13 +25,13 @@ class LikeController extends Controller
             'user_id' => auth()->id()
         ]);
 
-        broadcast(new ShippingStatusUpdated($reply->id, 1))->toOthers();
+        broadcast(new LikeEvent($reply->id, 1))->toOthers();
     }
 
     public function unLikeIt(Reply $reply)
     {
         $reply->like()->where('user_id', auth()->id())->first()->delete();
 
-        broadcast(new ShippingStatusUpdated($reply->id, 0))->toOthers();
+        broadcast(new LikeEvent($reply->id, 0))->toOthers();
     }
 }
