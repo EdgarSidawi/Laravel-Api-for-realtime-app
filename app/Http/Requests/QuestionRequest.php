12a1b90b1c528a921fb2f84100ba1cec3c77cdf9
Edgar Sidawi <romantibuai@gmail.com>
2019-11-23 20:58:12 +0000

    question request rules

--- app/Http/Requests/QuestionRequest.php
+++ app/Http/Requests/QuestionRequest.php
@@ -13,7 +13,7 @@ class QuestionRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class QuestionRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required',
+            'body' => 'required',
+            'category_id' => 'required',
         ];
     }
 }
